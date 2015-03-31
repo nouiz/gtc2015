b85917410ce7aea8a7a2b4b582840472a02f45da
Frederic <nouiz@nouiz.org>
2015-03-31 11:10:37 -0400

    Small update to use FAST_COMPILE

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# gtc2015
+# GTC2015
+
+This repository contains the material for the Theano presentation at
+GTC2015. It includes a small modification done after the presentation. It
+is highly recommended that you use Theano version 0.7 or higher.
+
+http://registration.gputechconf.com/quicklink/a9tmpNt
--- notebook/07_mode.py
+++ notebook/07_mode.py
@@ -2,9 +2,8 @@ import numpy as np
 from theano import function
 from theano import tensor as T
 from theano import config
-config.compute_test_value = 'raise'
+
 a = T.vector()
-a.tag.test_value = np.ones((3,)).astype(a.dtype)
 b = T.log(a)
 c = T.nnet.sigmoid(b)
 d = T.sqrt(c)
@@ -13,8 +12,5 @@ f = b * c * d
 # This is the first bad line
 g = e + f
 h = g / c
-fn = function([a], h)
+fn = function([a], h, mode='FAST_COMPILE')
 fn(np.ones((3,)).astype(a.dtype))
-
-# Reset it to default behavior for following exercices
-config.compute_test_value = 'off'
